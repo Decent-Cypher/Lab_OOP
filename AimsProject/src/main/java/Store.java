9026c29476cea8ae203fcf0d40c90ce74efc8e37
Decent-Cypher <binhngdc1911@gmail.com>
2024-04-27 20:45:19 +0700

    Add a Store class and implement the addDVD and removeDVD method and test them in the StoreTest class

---
+++ AimsProject/src/main/java/Store.java
@@ -0,0 +1,27 @@
+public class Store {
+    public static int MAX_QUANTITY = 50000;
+    private DigitalVideoDisc[] itemsInStore = new DigitalVideoDisc[MAX_QUANTITY];
+    private int quantity = 0;
+    public void addDVD(DigitalVideoDisc disc){
+        itemsInStore[quantity] = disc;
+        quantity++;
+        System.out.println("The disc has been added");
+    }
+    public void removeDVD(DigitalVideoDisc disc){
+        for (int i = 0; i < quantity; i++){
+            if (itemsInStore[i] == disc) {
+                for (int j = i; j < quantity - 1; j++) {
+                    itemsInStore[j] = itemsInStore[j + 1];
+                }
+                quantity--;
+                System.out.println("The disc has been removed");
+                break;
+            }
+        }
+    }
+    public void print(){
+        for (int i = 0; i < quantity; i++){
+            System.out.println(itemsInStore[i].toString());
+        }
+    }
+}
---
+++ AimsProject/src/main/java/StoreTest.java
@@ -0,0 +1,15 @@
+public class StoreTest {
+    public static void main(String[] args) {
+        Store store = new Store();
+        DigitalVideoDisc dvd1 = new DigitalVideoDisc ("The Exorcist", "Animation", "Roger Allers", 87, 19.95f);
+        DigitalVideoDisc dvd2 = new DigitalVideoDisc("Civil Wars", "Science Fiction", "George Lucas", 87, 24.95f);
+        DigitalVideoDisc dvd3 = new DigitalVideoDisc ("Ant Man", "Animation", 18.99f);
+        store.addDVD(dvd1);
+        store.addDVD(dvd2);
+        store.addDVD(dvd3);
+        store.print();
+        store.removeDVD(dvd1);
+        store.print();
+
+    }
+}
