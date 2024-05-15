9034ecc07ab9e8f3d874a1a3ac3b9120e97d69bc
Decent-Cypher <binhngdc1911@gmail.com>
2024-05-15 10:31:38 +0700

    Update the Store class to work with Media instead of DigitalVideoDisc

--- AimsProject/src/main/java/hust/soict/dsai/aims/store/Store.java
+++ AimsProject/src/main/java/hust/soict/dsai/aims/store/Store.java
@@ -1,31 +1,34 @@
 package hust.soict.dsai.aims.store;
 
 import hust.soict.dsai.aims.media.DigitalVideoDisc;
+import hust.soict.dsai.aims.media.Media;
+
+import java.util.ArrayList;
 
 public class Store {
     public static int MAX_QUANTITY = 50000;
-    private DigitalVideoDisc[] itemsInStore = new DigitalVideoDisc[MAX_QUANTITY];
-    private int quantity = 0;
-    public void addDVD(DigitalVideoDisc disc){
-        itemsInStore[quantity] = disc;
-        quantity++;
-        System.out.println("The disc has been added");
+    private ArrayList<Media> itemsInStore = new ArrayList<Media>();
+    public void addMedia(Media media){
+        if (itemsInStore.size() == MAX_QUANTITY){
+            System.out.println("The store is already full");
+            return;
+        }
+        itemsInStore.add(media);
+        System.out.println("The media was added to the store");
     }
-    public void removeDVD(DigitalVideoDisc disc){
-        for (int i = 0; i < quantity; i++){
-            if (itemsInStore[i] == disc) {
-                for (int j = i; j < quantity - 1; j++) {
-                    itemsInStore[j] = itemsInStore[j + 1];
-                }
-                quantity--;
-                System.out.println("The disc has been removed");
-                break;
-            }
+    public void removeMedia(Media media){
+        if (itemsInStore.contains(media)){
+            itemsInStore.remove(media);
+            System.out.println("The media was removed from the store");
+        }
+        else{
+            System.out.println("The media doesn't exist in the store");
         }
     }
-    public void print(){
-        for (int i = 0; i < quantity; i++){
-            System.out.println(itemsInStore[i].toString());
-        }
-    }
+
+//    public void print(){
+//        for (int i = 0; i < quantity; i++){
+//            System.out.println(itemsInStore[i].toString());
+//        }
+//    }
 }
