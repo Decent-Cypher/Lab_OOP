a214c65bfb51ea3298d55e7fde1fa4f96060253d
Decent-Cypher <binhngdc1911@gmail.com>
2024-05-31 04:33:02 +0700

    Update action for the remove button

--- AimsProject/src/main/java/hust/soict/dsai/aims/screen/CartScreenController.java
+++ AimsProject/src/main/java/hust/soict/dsai/aims/screen/CartScreenController.java
@@ -5,11 +5,13 @@ import hust.soict.dsai.aims.media.Media;
 import hust.soict.dsai.aims.media.Playable;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+
 
 public class CartScreenController {
     private Cart cart;
@@ -49,6 +51,11 @@ public class CartScreenController {
                 }
             }
         });
+    }
+    @FXML
+    void btnRemovePressed(ActionEvent event){
+        Media media = tblMedia.getSelectionModel().getSelectedItem();
+        cart.removeMedia(media);
     }
     void updateButtonBar(Media media){
         btnRemove.setVisible(true);
