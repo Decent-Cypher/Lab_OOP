cf815503682046ac556e8c1c44428d6cc8f28815
Decent-Cypher <binhngdc1911@gmail.com>
2024-05-31 04:23:17 +0700

    Update functionality for remove and play buttons

--- AimsProject/src/main/java/hust/soict/dsai/aims/screen/CartScreenController.java
+++ AimsProject/src/main/java/hust/soict/dsai/aims/screen/CartScreenController.java
@@ -2,7 +2,11 @@ package hust.soict.dsai.aims.screen;
 
 import hust.soict.dsai.aims.cart.Cart;
 import hust.soict.dsai.aims.media.Media;
+import hust.soict.dsai.aims.media.Playable;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
@@ -17,6 +21,10 @@ public class CartScreenController {
     private TableColumn<Media, String> colMediaCategory;
     @FXML
     private TableColumn<Media, Float> colMediaCost;
+    @FXML
+    private Button btnPlay;
+    @FXML
+    private Button btnRemove;
 
     public CartScreenController(Cart cart){
         super();
@@ -29,5 +37,26 @@ public class CartScreenController {
         colMediaCategory.setCellValueFactory(new PropertyValueFactory<Media, String>("category"));
         colMediaCost.setCellValueFactory(new PropertyValueFactory<Media, Float>("cost"));
         tblMedia.setItems(this.cart.getItemsOrdered());
+
+        btnPlay.setVisible(false);
+        btnRemove.setVisible(false);
+
+        tblMedia.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<Media>() {
+            @Override
+            public void changed(ObservableValue<? extends Media> observable, Media oldValue, Media newValue) {
+                if (newValue != null){
+                    updateButtonBar(newValue);
+                }
+            }
+        });
+    }
+    void updateButtonBar(Media media){
+        btnRemove.setVisible(true);
+        if(media instanceof Playable){
+            btnPlay.setVisible(true);
+        }
+        else{
+            btnPlay.setVisible(false);
+        }
     }
 }
